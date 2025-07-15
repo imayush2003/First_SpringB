d41a05e543eda94742b2ba7a3a240676a2527a3d
imayush2003 <154812665+imayush2003@users.noreply.github.com>
2025-07-15 20:44:35 +0530

    Updates.

--- FirstSpring/src/main/java/com/example/FirstSpring/Model/StudentDetails.java
+++ FirstSpring/src/main/java/com/example/FirstSpring/Model/StudentDetails.java
@@ -1,3 +1,4 @@
+
 package com.example.FirstSpring.Model;
 
 import jakarta.persistence.Column;
--- FirstSpring/src/main/java/com/example/FirstSpring/Repository/StudentRepository.java
+++ FirstSpring/src/main/java/com/example/FirstSpring/Repository/StudentRepository.java
@@ -1,9 +1,11 @@
 package com.example.FirstSpring.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.FirstSpring.Model.StudentDetails;
 
+@Repository
 public interface StudentRepository extends JpaRepository<StudentDetails, Integer>{
 
 }
